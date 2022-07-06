9d9347106da2c523bef8ec58e7058152271d85ee
Priyanka Sunkavalli <Priyanka.Sunkavalli@aveva.com>
2022-07-06 12:35:50 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Apiurl = "http://www.omdbapi.com/?apikey=2ac059dc";
+const App = () => {
+  const SearchMovies = async (title) => {
+    const response = await fetch(`${Apiurl}&s=${title}`);
+    const data = await response.json();
+    // fetch(`${Apiurl}&s=${title}`)
+    // .then(response=>response.json())
+    // .then(data=>)
+    console.log(data);
+  };
 
+  useEffect(() => {}, []);
+  return <h1>App</h1>;
+};
 export default App;
